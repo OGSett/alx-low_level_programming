15e2ee0688ee25c61a9163b941f7c80f06f45a5b
OGSett <ecomseth@gmail.com>
2023-07-11 10:23:26 -0700

    edit 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,34 +11,34 @@ int main(int ac, char **av)
 {
 	int from;
 	int to;
-	ssize_t read;
+	ssize_t readc;
 	char buffer[BUFFER_SIZE];
 
-	read = 0;
+	readc = 0;
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage : cp file_from file_to\n");
-		exite(97);
+		exit(97);
 	}
 	from = open(av[1], O_RDONLY);
 	if (from < 0)
 	{
-		dprintf(STEDEER_FILENO, "Error: can't read from file %s\n", av[1]);
-		exite(98);
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", av[1]);
+		exit(98);
 	}
 	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
-	while ((read = read(from, buffer, BUFFER_SIZE)) > 0)
+	while ((readc = read(from, buffer, BUFFER_SIZE)) > 0)
 	{
-		if (to < 0 || write(to, buffer, read) < 0)
+		if (to < 0 || write(to, buffer, readc) < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exite(99);
+			exit(99);
 		}
 	}
-	if (read < 0)
+	if (readc < 0)
 	{
-		dprintf(TDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exite(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
 	}
 	if (close(from) == -1 || close(to) == -1)
 	{
@@ -46,7 +46,7 @@ int main(int ac, char **av)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
 		if (close(to) == -1)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
-		exite(100);
+		exit(100);
 	}
 	return (0);
 }
